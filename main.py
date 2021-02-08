acc23ffc9bd2273a0051d98b58b81dc8defc9d3b
Fábio Pires <fabiopires1011@gmail.com>
2021-02-08 17:55:34 +0000

    On message event

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ client=discord.Client()
 @client.event
 async def on_ready():
   print('We have logged int as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+
+  #if the message is from the bot
+  if message.author==client.user:
+    return
+
+  #if the message is a command
+  if message.content.startswith('$hello'):
+    await message.channel.send('Hello')
+
+
